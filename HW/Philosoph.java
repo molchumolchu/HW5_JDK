db1b1aff591f1905dbf1e0569f79f60795eb001d
Aleks <sasha.molchanova94@yandex.com>
2024-10-14 17:55:50 +0300

    HW5

--- HW/Philosoph.java
+++ HW/Philosoph.java
@@ -8,22 +8,34 @@ import java.util.concurrent.CountDownLatch;
 
 public class Philosoph implements Runnable {
 
-    volatile ArrayList<Boolean> forks;
-    private CountDownLatch eatSignal;
-    private CountDownLatch thinkSignal;
+    // volatile static Boolean [] forks;
+    private volatile static Boolean[] forks = {true, true, true, true, true};
+    private static CountDownLatch eatSignal=new CountDownLatch(5);
+    private static CountDownLatch thinkSignal=new CountDownLatch(5);
     private int num;
     private int countEat = 1;
 
-    // private boolean leftFork;
-    // private boolean rightFork;
 
-    public Philosoph(int num, CountDownLatch eatSignal, CountDownLatch thinkSignal, ArrayList<Boolean> forks) {
+    public Philosoph(int num, CountDownLatch eatSignal, CountDownLatch thinkSignal) {
         this.num = num;
         this.eatSignal = eatSignal;
         this.thinkSignal = thinkSignal;
-        // this.leftFork=leftFork;
-        // this.rightFork=rightFork;
+        
         this.forks = forks;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Philosoph> philosophs = new ArrayList<>(Arrays.asList(
+            new Philosoph(1, eatSignal, thinkSignal),
+            new Philosoph(2, eatSignal, thinkSignal),
+            new Philosoph(3, eatSignal, thinkSignal),
+            new Philosoph(4, eatSignal, thinkSignal),
+            new Philosoph(5, eatSignal, thinkSignal)
+        ));
+
+        for (Philosoph philosoph: philosophs){
+            new Thread(philosoph).start();
+        }
     }
 
 
@@ -34,13 +46,11 @@ public class Philosoph implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        thinkSignal.countDown();
-        think(); //??
-        eat();
-        think();
-        eat();
-        think();
-        eat();
+        
+        while (countEat<=3) {
+            eat();
+            think();
+        }
         System.out.println("Филосов "+ this.num + " наелся до отвала и будет размышлять");
         think();
 
@@ -49,36 +59,30 @@ public class Philosoph implements Runnable {
 
     // метод размышлений
     private void think (){
-        try {
-            // go();
-            thinkSignal.await();
             System.out.println("Филосов "+ this.num + " приступил к размышлениям");
-            if (checkForks()==true) {
-                thinkSignal.countDown();//!
+            while(checkForks()==false){
+                go();
             }
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            if (checkForks()==true) {
+                thinkSignal.countDown();
+            }
     }
 
     //метод еды
     void eat (){
         try {
-            go();
-            getForks();
-            // System.out.println(" Филосов " + this.num +" взял вилки");
-            // eatSignal.await();
-            eatSignal.countDown();
-            System.out.println("Филосов " + this.num +" приступил к еде " + countEat +" раз");
-            Thread.sleep(new Random().nextInt(3000,5000));
+            if (checkForks()==true) {
+                getForks();
+                System.out.println("Филосов " + this.num +" приступил к еде " + countEat +" раз");
+                go();
+                Thread.sleep(new Random().nextInt(2000,5000));
+                eatSignal.countDown();
+                countEat++;
+                putForks();
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        // System.out.println("Филосов " + this.num +" закончил есть");
-        putForks();
-        countEat++;
     }
 
     public void go (){
@@ -88,12 +92,11 @@ public class Philosoph implements Runnable {
     }
 
     // проверяем левую (-1) и правую вилку(-2)
-    public boolean checkForks(){
-        
-        if (this.num == 1 && forks.get(0).equals(true) && forks.get(forks.size()-1).equals(true)) {
+    public synchronized boolean checkForks(){
+        if (num==1&&forks[0]==true && forks[4]==true){
             return true;
         }
-        if (this.num>=2 && forks.get(num-2).equals(true) && forks.get(num-1).equals(true)){
+        if (num!=1&&forks[num-1]==true && forks[num-2]==true){
             return true;
         }
         else{
@@ -101,47 +104,35 @@ public class Philosoph implements Runnable {
         }        
     }
 
-    // public boolean checkForks(){
-        
-    //     if(leftFork==true&&rightFork==true){
-    //         return true;
-    //     }        
-    //     else return false;
-    // }
 
     // берем вилку
     public void getForks(){
-        // leftFork = false;
-        // rightFork = false;
-        // System.out.println("Вилка " + (num-1) +" и "+"вилка " +(num) +" заняты Философом "+num);
-        
-        if (this.num == 1) {
-            forks.set(num-1, false);
-            forks.set(forks.size()-1, false);
-            System.out.println("Заняты Вилка " + (num) +" и "+"вилка " +(forks.size()) +" Философом "+num);
+        if(num==1){
+            forks[num-1]=false;
+            forks[4]=false;
+            System.out.println("Заняты Вилка " + (num-1) +" и "+"вилка " +(4) +" Философом "+num);
         }
-        if(this.num >=2) {
-            forks.set(num-2, false);
-            forks.set(num-1, false);
-            System.out.println("Заняты Вилка " + (num-1) +" и "+"вилка " +(num) +" Философом "+num);
-        }          
+        if(num!=1){
+            forks[num-1]=false;
+            forks[num-2]=false;
+            System.out.println("Заняты Вилка " + (num-1) +" и "+"вилка " +(num-2) +" Философом "+num);
+        }
+
+                
     }
 
     // кладем вилку
     public void putForks(){
-        // rightFork=true;
-        // leftFork=false;
-        // System.out.println("Вилка " + (num) +" и "+"вилка " +(num-1) +" освободились Философом "+num);
-        if (this.num == 1) {
-            forks.set(num-1, true);
-            forks.set(forks.size()-1, true);
-            System.out.println("Освободилась Вилка " + (num) +" и "+"вилка " +(5) +" Философом "+num);
+        if(num==1){
+            forks[num-1]=true;
+            forks[4]=true;
+            System.out.println("Освободились Вилка " + (num-1) +" и "+"вилка " +(4) +" Философом "+num);
         }
-        if(this.num >=2) {
-            forks.set(num-2, true);
-            forks.set(num-1, true);
-            System.out.println("Освободилась Вилка " + (num-1) +" и "+"вилка " +(num) +" Философом "+num);
-        }          
+        if(num!=1){
+            forks[num-1]=true;
+            forks[num-2]=true;
+            System.out.println("Освободились Вилка " + (num-1) +" и "+"вилка " +(num-2) +" Философом "+num);
+        }
     }
 
 }
